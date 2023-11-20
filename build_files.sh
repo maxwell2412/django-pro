531ae15c51393d416e67c9035eb7a47fa9239768
maxwell2412 <maxwell24121992@hotmail.com>
2023-11-20 15:27:42 -0300

    nova aplicação vercel 2

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 echo " BUILD START"
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
+
+echo " MAKE Migration..."
+python3.9 manage.py makemigrations --noinput 
+python3.9 manage.py migrate --noinput
+
 python3.9 manage.py collectstatic --noinput --clear 
 echo " BUILD END"
--- cafeteriaDev/settings.py
+++ cafeteriaDev/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +84,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -118,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles_builde" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
